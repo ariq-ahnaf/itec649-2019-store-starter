92f6824869991b3bcd6d09ab130e395b6f2325e2
Ariq Ahnaf Shaan <ariq-ahnaf.shaan@students.mq.edu.au>
2019-05-23 11:51:56 +1000

    new html pages created product_page.html added cart.html added

--- main.py
+++ main.py
@@ -10,14 +10,32 @@ app = Bottle()
 
 @app.route('/')
 def index(db):
-
+    data = model.product_list(db)
     info = {
         'title': "The WT Store",
+        'message': data,
     }
 
     return template('index', info)
 
+@app.route('/product/0')
+def index(db):
+    session.get_or_create_session(db)
 
+    data = model.product_get(db, 0)
+    info = {
+        'data': data
+    }
+    return template('product_page', info)
+
+@app.route('/product/1')
+def index(db):
+    session.get_or_create_session(db)
+    data = model.product_get(db,1)
+    info = {
+        'data': data
+    }
+    return template('product_page',info)
 
 @app.route('/static/<filename:path>')
 def static(filename):
